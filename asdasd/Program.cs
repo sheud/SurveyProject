4799b724e484a1a7e7887e4b1fb1adb8bb0432e4
sheud <Kim.Poutanen@student.lab.fi>
2020-11-23 13:04:01 +0200

    jotain

--- asdasd/Program.cs
+++ asdasd/Program.cs
@@ -6,6 +6,88 @@ using System.Threading.Tasks;
 
 namespace asdasd
 {
+    public class Survey
+    {
+        private string surveyTitle;
+        private string status;
+        private DateTime closedt;
+        private DateTime startdt;
+
+        public string SetSurvey()
+        {
+
+        }
+
+    public string SetQuestion()
+        {
+
+        }
+
+
+
+}
+
+    public class Manager
+    {
+        private int mID;
+        private string mName;
+        private int managerKey;
+
+        public string SetmName()
+        {
+
+        }
+    
+    public int GenerateKey()
+        {
+
+        }
+    
+    public void GetmKey() 
+        {
+            return managerKey;
+        }
+    
+
+    public class Database
+    {
+
+    }
+
+    public class Application
+    {
+
+    }
+
+    public class Admin
+    {
+
+    }
+
+    public class managerKey
+    {
+
+    }
+
+    public class respodentKey
+    {
+
+    }
+
+    public class Page
+    {
+
+    }
+
+    public class Question
+    {
+
+    }
+
+    public class Response
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
