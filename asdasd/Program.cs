2cbf54b7134a5a4cc5807ee1eb47dd0c409ff6e3
Jesse <sipari.jesse@gmail.com>
2020-12-06 17:32:41 +0200

    rungontynkä

--- asdasd/Program.cs
+++ asdasd/Program.cs
@@ -3,97 +3,129 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace asdasd
 {
-    class Program
+    enum UserLevel { Admin = 1, Manager = 2, Respodent = 3}
+
+    class Database
     {
-        private string surveyTitle;
-        private string status;
-        private DateTime closedt;
-        private DateTime startdt;
-
-        public string SetSurvey(string st)
+        MySqlConnection connection;
+        public Database()
         {
-            return status;
+            OpenConnection();
+        }
+        public void OpenConnection()
+        {
+            connection = new MySqlConnection("server=127.0.0.1;user=root;database=testbase;port=3306;password=");
+            connection.Open();
         }
 
-        public string SetQuestion()
+        public void Login()
         {
-            return status;
+            
         }
 
-        public class Manager
+        public void AddUserKey(string key)
         {
-            private int mID;
-            private string mName;
-            private int managerKey;
+            MySqlCommand addKeyCommand = connection.CreateCommand();
+            addKeyCommand.CommandText = "INSERT INTO asd (avain, userlevel) VALUES ('" + key + "'" + ",'"+ (int)UserLevel.Respodent + "');";//  + "WHERE name=@name;";
+            addKeyCommand.Parameters.AddWithValue("@avain", key);
+            MySqlDataReader results = addKeyCommand.ExecuteReader();
+            while (results.Read())
+            {
+                Console.WriteLine(results.GetString(0));
+            }
+
+            Console.ReadKey(true);
+            CloseConnection();
         }
 
-        public string SetmName()
+        //MySqlCommand addKeyCommand = connection.CreateCommand();
+        //addKeyCommand.CommandText = "INSERT INTO asd (avain, userLevel) VALUES " + "('" + key + "'" + "'" +
+        //(int) UserLevel.Respodent + "');";//  + "WHERE name=@name;";
+        //addKeyCommand.Parameters.AddWithValue("@name", "B");
+        //MySqlDataReader results = addKeyCommand.ExecuteReader();
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+    }
+    class Question
+    {
+
+    }
+
+    class Page
+    {
+        private List<Question> questions;
+    }
+
+    class Survey
+    {
+        private List<Page> pages;
+    }
+
+    class Application
+    {
+        Database db;
+        UserInterface ui;
+        private Survey survey;
+        private UserLevel currentUser;
+
+        public Application()
+        {
+            db = new Database();
+            ui = new UserInterface();
+            ui.Run(this);
+        }
+
+        public void Login()
+        {
+            //currentUser = db.Login(key);
+        }
+
+        public void AddRegularKey()
+        {
+            string key; // ehkä kysytään databasesta
+            key = Console.ReadLine();
+            db.AddUserKey(key);
+        }
+    }
+
+    class UserInterface
+    {
+        private Application app;
+
+        public void Run(Application _app)
+        {
+            app = _app;
+
+            Console.WriteLine("moikka 'Add'");
+            var command = Console.ReadLine();
+            while (true)
             {
-
-            }
-
-            public int GenerateKey()
-            {
-
-            }
-
-            public void GetmKey()
-            {
-                return managerKey;
-            }
-
-            public class Database
-            {
-
-            }
-
-            public class Application
-            {
-
-            }
-
-            public class Admin
-            {
-
-            }
-
-            public class managerKey
-            {
-
-            }
-
-            public class respodentKey
-            {
-
-            }
-
-            public class Page
-            {
-
-            }
-
-            public class Question
-            {
-
-            }
-
-            public class Response
-            {
-
-            }
-
-            class Program
-            {
-                static void Main(string[] args)
+                switch (command)
                 {
+                    case "Add":
+                        app.AddRegularKey();
+                        break;
                 }
             }
+        }
+    }
 
+    class Program
+    {
         static void Main(string[] args)
         {
+            Application app = new Application();
+           
         }
     }
 }
+
+
